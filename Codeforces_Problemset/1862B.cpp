9a04b0ce7e77d2542450db77d4a874d8fd6b70ba
UNKNOWNAR <amiarinjaysarkar@gmail.com>
2026-04-29 17:46:01 +0530

    Day-4

--- Codeforces_Problemset/1862B.cpp
+++ Codeforces_Problemset/1862B.cpp
@@ -6,7 +6,25 @@ using namespace std;
 #define all(v) v.begin(), v.end()
 #define endl '\n'
 void solve() {
-    
+    int n;
+    cin >> n;
+    int arr[n];
+    for (int i = 0; i < n; i++) 
+        cin >> arr[i];
+    vector<int> b;
+    b.pb(arr[0]);
+    for(int i=1;i<n;i++){
+        if(arr[i]>=arr[i-1])
+            b.pb(arr[i]);
+        else{
+            b.pb(arr[i]);
+            b.pb(arr[i]);
+        }
+    }
+    cout<<b.size()<<endl;
+    for(int i=0;i<b.size();i++) 
+        cout<<b[i]<< " ";
+    cout<<endl;     
 }
 
 int main() {
