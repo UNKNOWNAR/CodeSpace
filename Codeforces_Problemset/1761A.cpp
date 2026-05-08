5b86769bd6db0e6408da68257c2f4797f8004186
UNKNOWNAR <amiarinjaysarkar@gmail.com>
2026-05-08 12:34:48 +0530

    Day-13

---
+++ Codeforces_Problemset/1761A.cpp
@@ -0,0 +1,25 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define fast_io ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL)
+#define ll long long
+#define pb push_back
+#define all(v) v.begin(), v.end()
+#define endl '\n'
+void solve() {
+    int n,a,b;
+    cin>>n>>a>>b;
+    if((a==b && a==n)||(a+b+2<=n))
+        cout<<"YES"<<endl;
+    else
+        cout<<"NO"<<endl;
+}
+
+int main() {
+    fast_io; 
+    int t = 1;
+    cin >> t; 
+    while (t--) {
+        solve();
+    }
+    return 0;
+}
