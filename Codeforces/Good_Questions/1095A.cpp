442e001d012686022fce9fd1891f8b9bae99badc
UNKNOWNAR <amiarinjaysarkar@gmail.com>
2026-09-19 20:35:21 +0530

    Day-7A

--- Codeforces/Good_Questions/1095A.cpp
+++ Codeforces/Good_Questions/1095A.cpp
@@ -1,42 +1,26 @@
 #include <bits/stdc++.h>
 using namespace std;
 #define fast_io ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL)
-#define ll long long
-#define pb push_back
 #define all(v) v.begin(), v.end()
 #define endl '\n'
 void solve() {
-    int modulo =  676767677;
     int n;
     cin>>n;
-    vector <int> arr(n);
-    for(int i=0;i<n;i++) cin>>arr[i];
-    bool allOnes = true;
-    for(int i=0;i<n;i++){
-        if(arr[i]!=1) allOnes = false;
+    string s;
+    cin>>s;
+    int len = (int)sqrt(2*n);
+    string ans = "";
+    while(n>0){
+        ans += s[n-1];
+        n-=len;
+        len--;
     }
-    if(allOnes){
-        cout<<1<<endl;
-        return;
-    }
-    ll ans = 0;
-    int unmatchedOnes = 0;
-    for(int i = 0; i<n;i++){
-        if(arr[i]==1)
-            unmatchedOnes++;
-        else{
-            ans = (ans+arr[i])%modulo;
-            if(unmatchedOnes>0) unmatchedOnes--;
-        }   
-    }
-    ans = (ans+unmatchedOnes)%modulo;
+    reverse(all(ans));
     cout<<ans<<endl;
 }
-
 int main() {
     fast_io; 
-    int t = 1;
-    cin >> t; 
+    int t=1; 
     while (t--) {
         solve();
     }
