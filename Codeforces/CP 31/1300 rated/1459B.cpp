29e9cad8e67c9191d99f8daf5357da979ce8000b
UNKNOWNAR <amiarinjaysarkar@gmail.com>
2026-09-23 06:11:30 +0530

    Day-11

--- Codeforces/CP 31/1300 rated/1459B.cpp
+++ Codeforces/CP 31/1300 rated/1459B.cpp
@@ -5,58 +5,15 @@ using namespace std;
 #define pb push_back
 #define all(v) v.begin(), v.end()
 #define endl '\n'
-struct info{
-    int x;
-    int y;
-    int prev;
-};
-//0 - north
-//1 - south
-//2 - east
-//3 - west
-int dr[4] = {1,-1,0,0};
-int dc[4] = {0,0,1,-1};
 void solve() {
     int n;
     cin>>n;
-    queue<info> q;
-    q.push({0,0,-1});
-    int ans = 0;
-    for(int j=0;j<n;j++){
-        int siz = q.size();
-        set<pair<int,int>> newStates;
-        while(siz--){
-            auto [x,y,prev]=q.front();
-            q.pop();
-            if(j&1){
-                if(prev==0||prev==1){
-                    for(int i=2;i<4;i++){
-                        int nx=x+dr[i];
-                        int ny=y+dc[i];
-                        newStates.insert({nx,ny});
-                        q.push({nx,ny,i});
-                    }
-                }   
-                else{
-                    for(int i=0;i<2;i++){
-                        int nx=x+dr[i];
-                        int ny=y+dc[i];
-                        newStates.insert({nx,ny});
-                        q.push({nx,ny,i});
-                    }
-                }
-            }
-            else{
-                for(int i=0;i<4;i++){
-                    int nx=x+dr[i];
-                    int ny=y+dc[i];
-                    newStates.insert({nx,ny});
-                    q.push({nx,ny,i});
-                }
-            }
-        }     
-        ans = newStates.size();
-    }
+    ll ans=0;
+    int  k =n/2;
+    if(n&1)
+        ans = 2*(k+1)*(k+2);
+    else 
+        ans = (k+1)*(k+1);
     cout<<ans<<endl;
 }
 
